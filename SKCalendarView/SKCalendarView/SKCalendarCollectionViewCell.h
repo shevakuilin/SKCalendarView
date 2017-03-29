34e93ded6a1c725235fe01546e2dee3ae1647e84
ShevaKuilin <shevakuilin@gmail.com>
2017-03-29 16:50:43 +0800

    creat calendarItem

---
+++ SKCalendarView/SKCalendarView/SKCalendarCollectionViewCell.h
@@ -0,0 +1,23 @@
+//
+//  SKCalendarCollectionViewCell.h
+//  SKCalendarView
+//
+//  Created by shevchenko on 17/3/29.
+//  Copyright © 2017年 shevchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKCalendarCollectionViewCell : UICollectionViewCell
+@property (nonatomic, strong) NSString * calendarDate;// 日期
+@property (nonatomic, strong) UIColor * calendarDateColor;// 日期字体颜色
+@property (nonatomic, strong) UIColor * dateColor;// 日期颜色
+@property (nonatomic, strong) UIImage * dateIcon;// 日期图片
+@property (nonatomic, strong) UIColor * dateBackgroundColor;// 日期背景颜色
+@property (nonatomic, strong) UIImage * dateBackgroundIcon;// 日期背景图片
+@property (nonatomic, strong) NSString * calendarTitle;// 日期标题
+@property (nonatomic, strong) UIColor * calendarTitleColor;// 日期标题字体颜色
+@property (nonatomic, assign) BOOL enableClickEffect;// 开启点击效果
+@property (nonatomic, assign) BOOL enableDateRoundCorner;// 开启日期圆角
+
+@end
---
+++ SKCalendarView/SKCalendarView/SKCalendarCollectionViewCell.m
@@ -0,0 +1,161 @@
+//
+//  SKCalendarCollectionViewCell.m
+//  SKCalendarView
+//
+//  Created by shevchenko on 17/3/29.
+//  Copyright © 2017年 shevchenko. All rights reserved.
+//
+
+#import "SKCalendarCollectionViewCell.h"
+#import "SKConstant.h"
+
+@interface SKCalendarCollectionViewCell ()
+@property (nonatomic, strong) UIView * baseView;// 底部view
+@property (nonatomic, strong) UIImageView * backgroundIcon;// 背景图片
+@property (nonatomic, strong) UIImageView * icon;// 日期图片
+@property (nonatomic, strong) UILabel * dateLabel;// 日期
+@property (nonatomic, strong) UILabel * titleLabel;// 日期标题
+
+@end
+
+@implementation SKCalendarCollectionViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if ([super initWithFrame:frame]) {
+        if (self) {
+            [self customView];
+        }
+    }
+    
+    return self;
+}
+
+#pragma mark - 创建界面
+- (void)customView
+{
+    // 底部view
+    self.baseView = [UIView new];
+    [self addSubview:self.baseView];
+    self.baseView.backgroundColor = self.backgroundColor;
+    [self.baseView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self).with.insets(UIEdgeInsetsMake(0, 0, 0, 0));
+    }];
+    
+    // 背景图片
+    self.backgroundIcon = [UIImageView new];
+    [self.baseView addSubview:self.backgroundIcon];
+    [self.backgroundIcon mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.baseView).with.insets(UIEdgeInsetsMake(0, 0, 0, 0));
+    }];
+    
+    // 日期图片
+    self.icon = [UIImageView new];
+    [self.baseView addSubview:self.icon];
+    [self.icon mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.baseView);
+        make.top.equalTo(self).with.offset(5);
+        
+        make.size.mas_offset(CGSizeMake(35, 35));
+    }];
+    
+    // 日期
+    self.dateLabel = [UILabel new];
+    [self.baseView addSubview:self.dateLabel];
+    self.dateLabel.font = [UIFont systemFontOfSize:15];
+    self.dateLabel.textAlignment = NSTextAlignmentCenter;
+    [self.dateLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.icon);
+        make.left.equalTo(self.icon);
+        make.right.equalTo(self.icon);
+    }];
+    
+    // 日期标题
+    self.titleLabel = [UILabel new];
+    [self.baseView addSubview:self.titleLabel];
+    self.titleLabel.font = [UIFont systemFontOfSize:10];
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.icon);
+        make.bottom.equalTo(self);
+        make.left.equalTo(self);
+        make.right.equalTo(self);
+    }];
+}
+
+#pragma mark - 外部设置
+- (void)setCalendarDate:(NSString *)calendarDate
+{
+    _calendarDate = calendarDate;
+    if (!isEmpty(calendarDate)) {
+        self.dateLabel.text = calendarDate;
+    }
+}
+
+- (void)setCalendarDateColor:(UIColor *)calendarDateColor
+{
+    _calendarDateColor = calendarDateColor;
+    if (!isEmpty(calendarDateColor)) {
+        self.dateLabel.textColor = calendarDateColor;
+    }
+}
+
+- (void)setDateColor:(UIColor *)dateColor
+{
+    _dateColor = dateColor;
+    if (!isEmpty(dateColor)) {
+        self.icon.backgroundColor = dateColor;
+    }
+}
+
+- (void)setDateIcon:(UIImage *)dateIcon
+{
+    _dateIcon = dateIcon;
+    if (!isEmpty(dateIcon)) {
+        self.icon.image = dateIcon;
+    }
+}
+
+- (void)setDateBackgroundColor:(UIColor *)dateBackgroundColor
+{
+    _dateBackgroundColor = dateBackgroundColor;
+    if (!isEmpty(dateBackgroundColor)) {
+        self.backgroundIcon.backgroundColor = dateBackgroundColor;
+    }
+}
+
+-(void)setDateBackgroundIcon:(UIImage *)dateBackgroundIcon
+{
+    _dateBackgroundIcon = dateBackgroundIcon;
+    if (!isEmpty(dateBackgroundIcon)) {
+        self.backgroundIcon.image = dateBackgroundIcon;
+    }
+}
+
+- (void)setCalendarTitle:(NSString *)calendarTitle
+{
+    _calendarTitle = calendarTitle;
+    if (!isEmpty(calendarTitle)) {
+        self.titleLabel.text = calendarTitle;
+    }
+}
+
+- (void)setCalendarTitleColor:(UIColor *)calendarTitleColor
+{
+    _calendarTitleColor = calendarTitleColor;
+    if (!isEmpty(calendarTitleColor)) {
+        self.titleLabel.textColor = calendarTitleColor;
+    }
+}
+
+- (void)setEnableClickEffect:(BOOL)enableClickEffect
+{
+    _enableClickEffect = enableClickEffect;
+}
+
+- (void)setEnableDateRoundCorner:(BOOL)enableDateRoundCorner
+{
+    _enableDateRoundCorner = enableDateRoundCorner;
+}
+
+@end
